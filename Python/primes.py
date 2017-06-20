e9602a821a90c1ae94814aa9341b0822c690dcc9
Ten <tsangten@live.com>
2017-06-20 19:53:29 +0800

    Update primes.py
    add is_palindrome

--- Python/primes.py
+++ Python/primes.py
@@ -20,3 +20,10 @@ for n in primes():
         print n
     else:
         break
+
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+output = filter(is_palindrome, range(1, 1000))
+print(list(output))
