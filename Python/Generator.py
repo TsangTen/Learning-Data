f1ee161a8ac303e6b78c9f6c9c0c5bb40654004d
Ten <tsangten@live.com>
2017-06-20 17:33:49 +0800

    add yanghui triangles function

--- Python/Generator.py
+++ Python/Generator.py
@@ -8,3 +8,39 @@ def fib(max):
 		a, b = b, a + b
 		n = n + 1
 	return 'done'
+
+
+def triangles():
+    n, a = 1, 1
+    l = list()
+    while True:
+        ll = l
+        if n % 2 != 0:
+            if n == 1:
+                l.append(a)
+            else:
+                # return
+                m = n / 2 + 1
+                l = l [:m]
+                for i in range(1, m):
+                    l[i] = ll[i-1] + ll[i]
+                ll = l[:-1]
+                ll.reverse()
+                l.extend(ll)
+        else:
+            m = n / 2
+            if m == 1:
+                l.append(a)
+            else:
+                # return
+                l = l [:m]
+                for i in range(1, m):
+                    l[i] = ll[i-1] + ll[i]
+                ll = l[:]
+                ll.reverse()
+                l.extend(ll)
+        n = n + 1
+        yield l
+        # print l
+        # if n == 11:
+        #     break
