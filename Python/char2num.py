d3ac4be374755be7267240db34ce4116bacdc993
Ten <tsangten@live.com>
2017-06-20 19:13:02 +0800

    Update char2num.py
    add str2int

--- Python/char2num.py
+++ Python/char2num.py
@@ -7,3 +7,15 @@ def char2num(s):
     return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
 
 print type(reduce(fn, map(char2num, '13579')))
+
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+
+    return reduce(fn, map(char2num, '13579'))
+
+print type(str2int('24678'))
